1815b2bd4b06ad90b61734ffd5d466b44855b6f5
anaberia <ana.beria3@gmail.com>
2023-02-04 00:38:40 +0400

    added first/last name inputs and comments

--- src/components/PersonalInfo/PersonalInfo.js
+++ src/components/PersonalInfo/PersonalInfo.js
@@ -4,17 +4,41 @@ import "./PersonalInfo.css";
 function PersonalInfo() {
   return (
     <div className="PersonalInfo">
-      {/* leftside */}
       <div className="PersonalInfo-left">
-        <div className="PersonalInfo-backBtn">
-          <div className="vector">{"<"}</div>
+        {/* header */}
+        <div className="PersonalInfo-headerContainer">
+          <div className="PersonalInfo-backBtn">
+            <div className="vector">{"<"}</div>
+          </div>
+          <div className="PersonalInfo-header">
+            <div className="Header-txt">პირადი ინფო</div>
+            <div className="Header-pg">1/3</div>
+          </div>
         </div>
-        <div className="PersonalInfo-header">
-          <div className="Header-txt">პირადი ინფო</div>
-          <div className="Header-pg">1/3</div>
+        {/* header */}
+
+        {/* input */}
+        <div className="Input-container">
+          <div className="Name-lastName">
+            <div className="Name">სახელი</div>
+            <div className="LastName">გვარი</div>
+          </div>
+          <div className="Input-containerHeader">
+            <input type="text" className="NameInput" placeholder="ანზორ" />
+            <input
+              type="text"
+              className="LastNameInput"
+              placeholder="მუმლაძე"
+            />
+          </div>
+          <div className="Name-lastName ">
+            <div className="Name">მინიმუმ 2 ასო, ქართული ასოები</div>
+            <div className="LastName">მინიმუმ 2 ასო, ქართული ასოები</div>
+          </div>
         </div>
       </div>
-      {/* leftside */}
+      {/* input */}
+
       <div className="PersonalInfo-right">Right side</div>
     </div>
   );
