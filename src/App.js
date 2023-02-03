4dda087a9dc726eccaf70b8250cef07f78e90394
anaberia <ana.beria3@gmail.com>
2023-02-03 19:28:08 +0400

    Done Homepage styles

--- src/App.js
+++ src/App.js
@@ -1,7 +1,20 @@
 import "./App.css";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <div className="App-header">
+        <div className="App-logo1">
+          <img src="./images/logo1.png" alt="logo1" />
+        </div>
+      </div>
+
+      <div className="App-logo2">
+        <img src="./images/logo2.png" alt="logo2" />
+      </div>
+      <button className="App-addBtn">რეზიუმეს დამატება</button>
+    </div>
+  );
 }
 
 export default App;
