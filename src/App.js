5c3b1ea2f9f1ceb2e132c7712e0c99cb49177716
anaberia <ana.beria3@gmail.com>
2023-02-03 19:57:24 +0400

    started working on personal info page structure

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import "./App.css";
+import PersonalInfo from "./components/PersonalInfo/PersonalInfo";
 
 function App() {
   return (
     <div className="App">
-      <div className="App-header">
+      <PersonalInfo />
+      {/* <div className="App-header">
         <div className="App-logo1">
           <img src="./images/logo1.png" alt="logo1" />
         </div>
@@ -12,7 +14,7 @@ function App() {
       <div className="App-logo2">
         <img src="./images/logo2.png" alt="logo2" />
       </div>
-      <button className="App-addBtn">რეზიუმეს დამატება</button>
+      <button className="App-addBtn">რეზიუმეს დამატება</button> */}
     </div>
   );
 }
---
+++ src/components/PersonalInfo/PersonalInfo.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./PersonalInfo.css";
+
+function PersonalInfo() {
+  return (
+    <div className="PersonalInfo">
+      <div className="PersonalInfo-left">Left</div>
+      <div className="PersonalInfo-right">Right</div>
+    </div>
+  );
+}
+
+export default PersonalInfo;
