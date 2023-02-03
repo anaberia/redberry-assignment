584a3cb5a7d04044e4d4bafa668405f24c325b32
anaberia <ana.beria3@gmail.com>
2023-02-03 21:28:01 +0400

    added personal info header

--- src/components/PersonalInfo/PersonalInfo.js
+++ src/components/PersonalInfo/PersonalInfo.js
@@ -4,8 +4,18 @@ import "./PersonalInfo.css";
 function PersonalInfo() {
   return (
     <div className="PersonalInfo">
-      <div className="PersonalInfo-left">Left</div>
-      <div className="PersonalInfo-right">Right</div>
+      {/* leftside */}
+      <div className="PersonalInfo-left">
+        <div className="PersonalInfo-backBtn">
+          <div className="vector">{"<"}</div>
+        </div>
+        <div className="PersonalInfo-header">
+          <div className="Header-txt">პირადი ინფო</div>
+          <div className="Header-pg">1/3</div>
+        </div>
+      </div>
+      {/* leftside */}
+      <div className="PersonalInfo-right">Right side</div>
     </div>
   );
 }
